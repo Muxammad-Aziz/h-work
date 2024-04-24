2c140239dd7304760047ff5f596f4c12e7534fad
unknown <github.com>
2024-04-24 16:50:59 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+// class Animal {
+//     constructor(species, eat) {
+//         this.species = species
+//         this.eat = eat
+//     }
+
+//     start() {
+//         return this.species + " хищник"
+//     }
+
+//     stop() {
+//         return this.species + " мясо"
+//     }
+// }
+
+// let species = prompt("Введите дикого животного:")
+
+
+// let wildAnimals = new Animal("Хищник", "Тигр")
+
+// console.log(
+//     wildAnimals.start(), "\n",
+//     wildAnimals.stop()
+// );
+
+
+// class Animal {
+//     constructor(species, eat) {
+//         this.species = species;
+//         this.eat = eat;
+//     }
+
+//     start() {
+//         return this.species + " " + this.species;
+//     }
+
+//     stop() {
+//         return this.species + " " + this.eat;
+//     }
+// }
+
+// let species = prompt("Введите животного:");
+// let eat = prompt("Чем питается " + "?");
+
+// let wildAnimal = new Animal(species, eat);
+
+// console.log(
+//     wildAnimal.start(), "\n",
+//     wildAnimal.stop()
+// );
